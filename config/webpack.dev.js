ce83c0c3ca421dfc60c0dc419dd9d188d8266269
youssefwork <youssef.samih97@gmail.com>
2020-01-12 21:10:42 +0100

    begin integrating sass less

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -41,7 +41,7 @@ module.exports = {
           {
             loader: "file-loader",
             options: {
-              name: "images/[name].[ext]"
+              name: "images/[name]-[hash:8].[ext]"
             }
           }
         ]
@@ -53,13 +53,33 @@ module.exports = {
             loader: "html-loader"
           }
         ]
+      },
+      {
+        test: /\.pug$/,
+        use: [
+          {
+            loader: "pug-loader"
+          }
+        ]
+      },
+      {
+        test: /\.hbs$/,
+        use: [
+          {
+            loader: "handlebars-loader",
+            query: {
+              inlineRequires: "/images/"
+            }
+          }
+        ]
       }
     ]
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new HTMLWebpackPlugin({
-      template: "./src/index.html"
+      template: "./src/index.hbs",
+      title: "Link's journal "
     })
   ]
 }
